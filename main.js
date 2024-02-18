bf1da9f2aeb794b967552d5a2aa2dc877ef24a99
nishitjainalt <nijain@tcd.ie>
2024-02-18 15:51:23 +0000

    final

--- main.js
+++ main.js
@@ -44,6 +44,90 @@ title.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
 
+const page = document.body;
+const toggle = page.querySelector(".toggle-input");
+const toggleIcon = page.querySelector(".toggle-icon i");
+
+// set theme and localStorage on page load
+setCheckedState();
+
+function setCheckedState() {
+  // checks if localStorage has a "checked" value set at all
+  if (!(localStorage.checked === undefined)) {
+    // if it does, it sets the state of the toggle accordingly
+    toggle.checked = isTrue(localStorage.getItem("checked"));
+    // after setting the toggle state, the theme is adjusted according to the checked state
+    toggleTheme();
+  }
+}
+
+function toggleTheme() {
+  // Toggle theme based on state of checkbox
+  replaceClass();
+  // replace icons on page
+  toggleIconTheme();
+  // set the value of the "checked" key in localStorage
+  updateLocalStorage();
+}
+
+function replaceClass() {
+  if (toggle.checked) {
+    page.classList.replace("light", "dark");
+  } else {
+    page.classList.replace("dark", "light");
+  }
+}
+
+function toggleIconTheme() {
+  // Replace icons not able to be targeted by css variables
+  if (page.classList.contains("light")) {
+    toggleIcon.classList.replace("far", "fas");
+  } else {
+    toggleIcon.classList.replace("fas", "far");
+  }
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("checked", toggle.checked);
+}
+
+function isTrue(value) {
+  // convert string to boolean
+  return value === "true";
+}
+
+// Toggle theme any time the state of the checkbox changes
+toggle.addEventListener("change", toggleTheme);
+
+const btn = document.getElementById("button");
+const thename = document.getElementById("from_name");
+const email = document.getElementById("email_id");
+const themessage = document.getElementById("message");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_53zvw5r";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Submit";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Submit";
+      alert(JSON.stringify(err));
+    }
+  );
+
+  thename.value = "";
+  email.value = "";
+  themessage.value = "";
+});
+
 {
   AOS.init({
     duration: 3000,
